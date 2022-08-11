704a9f8d14e92852a310b787a8d54fc40cc10820
Artur Baumeister <Artur.baumeister@gmail.com>
2022-08-11 16:46:06 +0200

    initial commit

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,51 @@
+const movies2 = [
+  {
+    title: "Fargo",
+    year: 1996,
+    director: "Joel Coen",
+    duration: "1h 38min",
+    genre: ["Crime", "Drama", "Thriller"],
+    score: 8.1,
+  },
+  {
+    title: "The Sixth Sense",
+    year: 1999,
+    director: "M. Night Shyamalan",
+    duration: "1h 47min",
+    genre: ["Drama", "Mystery", "Thriller"],
+    score: 8.1,
+  },
+  {
+    title: "The Thing",
+    year: 2004,
+    director: "John Carpenter",
+    duration: "1h 49min",
+    genre: ["Horror", "Mystery", "Sci-Fi"],
+    score: 8.2,
+  },
+  {
+    title: "No Country for Old Men",
+    year: 2007,
+    director: "Ethan Coen",
+    duration: "2h 2min",
+    genre: ["Crime", "Drama", "Thriller"],
+    score: 8.1,
+  },
+];
 // Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+// _Bonus_: It seems some of the directors had directed multiple movies so they will
+// pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+function getAllDirectors(moviesArray) {
+  console.log(moviesArray[1].director);
+  let directorArr = [];
+  moviesArray.map((movies) => {
+    directorArr.push(movies.director);
+  });
+  return directorArr;
+}
+console.log(getAllDirectors(movies2));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {}
